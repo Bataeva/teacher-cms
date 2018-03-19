a27b70540c6818d4c84e91340383192dd01a869e
Bataeva <bataeva09@iclud.com>
2018-03-19 12:52:15 +0300

    Enable/disable user registration in config

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/blog/search', 'BlogController@search')->name('search');
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth'] ], function () {
     Route::get('/', 'DashboardController@dashboard')->name('admin.index');
     Route::resource('/category', 'CategoryController', ['as'=>'admin']);
-     Route::resource('/article', 'ArticleController', ['as'=>'admin']);
-     Route::group(['prefix' => 'user_management', 'namespace' => 'UserManagement'], function () {
+    Route::resource('/article', 'ArticleController', ['as'=>'admin']);
+    Route::group(['prefix' => 'user_management', 'namespace' => 'UserManagement'], function () {
         Route::resource('/user', 'UserController', ['as' => 'admin.user_management']);
     });
 
@@ -26,5 +26,23 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['aut
 Route::get('/', function () {
     return view('blog.home');
 });
-Auth::routes();
+// Auth::routes();
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+// Registration Routes...
+if (config('auth.registration')) {
+    $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    $this->post('register', 'Auth\RegisterController@register');
+} else {
+    Route::match(['get','post'], 'register', function () {
+        return redirect()->route('login');
+    })->name('register');
+}
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::get('/home', 'HomeController@index')->name('home');
